d86ce934873d009eeb339c5a63d397ce12052516
chirsworking <aurion@student.telkomuniversity.ac.id>
2019-11-20 07:35:48 +0700

    🏠add homepage Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
+import HomePage from "./components/homepage/homepage.component";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
